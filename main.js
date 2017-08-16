5529f9837decd5b127be20538e56d91a52b596ac
Matt Hensley <matt.hensley777@gmail.com>
2017-08-15 19:46:00 -0500

    done

--- main.js
+++ main.js
@@ -40,10 +40,93 @@ console.log(backwardFruitsOld(fruits));
 
 // 2B: Rewrite using map array method
 
+var secondNewArray = fruits.map(function(fruit){
+	return fruit.split('').reverse().join('');
+});
+console.log(secondNewArray);
+
+
+// with a callback function
+
+// function splitter(item) {
+// 	returns item.split('').reverse().join('');
+// }
+// console.log("splitter", fruits.map(splitter));
+
 // 3A: Write a function called fiveLettersOnlyOld that only returns fruits that have 5 letters.
+
+function fiveLettersOnlyOld (array) {
+	var newArray = [];
+	for(var i = 0; i < array.length;i++) {
+		if(array[i].length === 5) {
+			newArray.push(array[i]);
+		}
+	}
+	return newArray;
+}
+console.log(fiveLettersOnlyOld(fruits));
+
 // 3B: Rewrite using filter array method
+
+var filteredFruit = fruits.filter(function(fruit){
+	if(fruit.length === 5) {
+		return true;
+	}
+});
+console.log("filtered fruit", filteredFruit);
 
 var numbers = [0, 1, 2, 3, 4];
 
 // 4A: Write a function called addzOld that returns the sum of an array.
+
+function addzOld(array) {
+	var sum = 0;
+	for(var i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
+console.log("doing the adding of numbers", addzOld(numbers));
+
 // 4B: Rewrite using reduce array method
+
+var summation = numbers.reduce(function(accumulation, currentValue) {
+	return accumulation + currentValue;
+});
+
+console.log("summation", summation);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
